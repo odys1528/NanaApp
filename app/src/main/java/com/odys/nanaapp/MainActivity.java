519f7366786c8a8c74fb6bb3db2bd1fd7c29226a
odys1528 <jolanta.zakrzewska@orange.pl>
2017-07-25 21:10:57 +0200

    navigation fragments

--- app/src/main/java/com/odys/nanaapp/MainActivity.java
+++ app/src/main/java/com/odys/nanaapp/MainActivity.java
@@ -1,30 +1,32 @@
 package com.odys.nanaapp;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_events:
-                    mTextMessage.setText(R.string.title_events);
+                    transaction.replace(R.id.content, new EventsFragment()).commit();
                     return true;
                 case R.id.navigation_members:
-                    mTextMessage.setText(R.string.title_members);
+                    transaction.replace(R.id.content, new MembersFragment()).commit();
                     return true;
                 case R.id.navigation_qrcode:
-                    mTextMessage.setText(R.string.title_qrcode);
+                    transaction.replace(R.id.content, new QRCodeFragment()).commit();
                     return true;
             }
             return false;
@@ -37,9 +39,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content, new EventsFragment()).commit();
     }
 
 }
