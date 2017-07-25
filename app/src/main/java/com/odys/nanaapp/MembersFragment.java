daaa3935832ee488b904097e0d48a3fdf07f4535
odys1528 <jolanta.zakrzewska@orange.pl>
2017-07-25 22:45:50 +0200

    cardview and recyclerview layouts

--- app/src/main/java/com/odys/nanaapp/MembersFragment.java
+++ app/src/main/java/com/odys/nanaapp/MembersFragment.java
@@ -7,8 +7,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 
 /**
@@ -20,12 +18,10 @@ import android.widget.Toast;
  * create an instance of this fragment.
  */
 public class MembersFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -43,7 +39,6 @@ public class MembersFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment MembersFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static MembersFragment newInstance(String param1, String param2) {
         MembersFragment fragment = new MembersFragment();
         Bundle args = new Bundle();
@@ -66,18 +61,13 @@ public class MembersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_members, container, false);
-        Button button = (Button) view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(),"nana",Toast.LENGTH_SHORT).show();
-            }
-        });
+
+        // TODO:  recyclerview and cardview
 
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -109,7 +99,6 @@ public class MembersFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
