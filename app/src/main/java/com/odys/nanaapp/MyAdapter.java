684b94d9abbd713073f6fa4633209126e57ae1f0
odys1528 <jolanta.zakrzewska@orange.pl>
2017-07-26 13:47:16 +0200

    changing 'sides' of cardview

--- app/src/main/java/com/odys/nanaapp/MyAdapter.java
+++ app/src/main/java/com/odys/nanaapp/MyAdapter.java
@@ -1,24 +1,52 @@
 package com.odys.nanaapp;
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] dataSet;
+    private static String[] dataSet;
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         public CardView cardView;
-        public TextView textView;
+        public TextView memberName;
+        public TextView memberDescription;
+        public ImageView memberPhoto;
+
         public MyViewHolder(View v) {
             super(v);
 
             cardView = (CardView) v.findViewById(R.id.card_view);
-            textView = (TextView) v.findViewById(R.id.memberName);
+            memberName = (TextView) v.findViewById(R.id.memberName);
+            memberDescription = (TextView) v.findViewById(R.id.memberDescription);
+            memberPhoto = (ImageView) v.findViewById(R.id.memberPhoto);
+
+            final boolean cardSide[] = new boolean[dataSet.length];
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!cardSide[getAdapterPosition()]) {
+                        memberName.setTextColor(ContextCompat.getColor(v.getContext(), R.color.colorPrimary));
+                        cardSide[getAdapterPosition()]=true;
+                        memberPhoto.setVisibility(View.INVISIBLE);
+                        memberDescription.setVisibility(View.VISIBLE);
+                    } else {
+                        memberName.setTextColor(ContextCompat.getColor(v.getContext(), android.R.color.tab_indicator_text));
+                        cardSide[getAdapterPosition()]=false;
+                        memberPhoto.setVisibility(View.VISIBLE);
+                        memberDescription.setVisibility(View.INVISIBLE);
+                    }
+                }
+            });
         }
     }
 
@@ -35,7 +63,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.textView.setText(dataSet[position]);
+        holder.memberName.setText(dataSet[position]);
     }
 
     @Override
